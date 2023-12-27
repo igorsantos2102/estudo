f8ead90245c015b3d2c3a0dc2d518a6cfadaa2ee
igorsantos2102 <igorsantos36912@gmail.com>
2023-12-27 20:29:25 -0300

    exercicios
    estruturas em python

---
+++ python/exercicios/002/estrutura_selecao.py
@@ -0,0 +1,15 @@
+n1 = 10
+n2 = 4
+n3 = 6
+n4 = 2
+
+soma = n1 + n2 + n3 + n4
+media = soma/4
+
+print("A media e: ", media)
+if media >=7:
+    print("Aprovado")
+elif  media < 5:
+    print("Reprovado")
+else:
+    print("Recuperacao")
---
+++ python/exercicios/003/estrutura_match.py
@@ -0,0 +1,21 @@
+dia = int (input("Digite o numero do dia da semana: "))
+
+match dia:
+    case 1:
+        print("Domingo")
+    case 2:
+        print("Segunda")
+    case 3:
+        print("Terca")
+    case 4:
+        print("Quarta")
+    case 5:
+        print("Quinta")
+    case 6:
+        print("Sexta")
+    case 7:
+        print("Sabado")
+    case other:
+        print("Esse dia nao existe")
+
+   
